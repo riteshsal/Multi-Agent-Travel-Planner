0f46cb96c3c318bc05f00d9c62e96c988b9553ed
riteshsal <riteshsalve86@gmail.com>
2026-09-16 18:54:11 +0530

    added frontend and backend

--- tes.py
+++
@@ -1,7 +0,0 @@
-from tools.tavily_tool import tavily_search
-from tools.flight_tool import search_flights
-#res=tavily_search("best Hotels in india")
-#print(res)
-
-res=search_flights("Plan a 7 days Japan trip from India")
-print(res)
